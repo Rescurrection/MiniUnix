e83c381c2ef140fd05b175f13425c7e98a6726c6
Rescurrection <358638311@qq.com>
2017-05-16 17:24:06 +0800

    Update pipe.c

--- shell/pipe.c
+++ shell/pipe.c
@@ -1,1 +1,36 @@
 
+#include "pipe.h"
+#include "memory.h"
+#include "fs.h"
+#include "file.h"
+#include "ioqueue.h"
+#include "thread.h"
+
+/* 判断文件描述符local_fd是否是管道 */
+bool is_pipe(uint32_t local_fd) {
+   uint32_t global_fd = fd_local2global(local_fd); 
+   return file_table[global_fd].fd_flag == PIPE_FLAG;
+}
+
+/* 创建管道,成功返回0,失败返回-1 */
+int32_t sys_pipe(int32_t pipefd[2]) {
+   int32_t global_fd = get_free_slot_in_global();
+
+   /* 申请一页内核内存做环形缓冲区 */
+   file_table[global_fd].fd_inode = get_kernel_pages(1); 
+
+   /* 初始化环形缓冲区 */
+   ioqueue_init((struct ioqueue*)file_table[global_fd].fd_inode);
+   if (file_table[global_fd].fd_inode == NULL) {
+      return -1;
+   }
+  
+   /* 将fd_flag复用为管道标志 */
+   file_table[global_fd].fd_flag = PIPE_FLAG;
+
+   /* 将fd_pos复用为管道打开数 */
+   file_table[global_fd].fd_pos = 2;
+   pipefd[0] = pcb_fd_install(global_fd);
+   pipefd[1] = pcb_fd_install(global_fd);
+   return 0;
+}
