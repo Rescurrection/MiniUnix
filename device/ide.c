34f0b8fc64af27f183749a9183413a500b08c932
Rescurrection <358638311@qq.com>
2017-05-23 20:28:10 +0800

    Create ide.c

---
+++ device/ide.c
@@ -0,0 +1,68 @@
+#include "ide.h"
+#include "sync.h"
+#include "io.h"
+#include "stdio.h"
+#include "stdio-kernel.h"
+#include "interrupt.h"
+#include "memory.h"
+#include "debug.h"
+#include "console.h"
+#include "timer.h"
+#include "string.h"
+#include "list.h"
+
+/* 定义硬盘各寄存器的端口号 */
+#define reg_data(channel)	 (channel->port_base + 0)
+#define reg_error(channel)	 (channel->port_base + 1)
+#define reg_sect_cnt(channel)	 (channel->port_base + 2)
+#define reg_lba_l(channel)	 (channel->port_base + 3)
+#define reg_lba_m(channel)	 (channel->port_base + 4)
+#define reg_lba_h(channel)	 (channel->port_base + 5)
+#define reg_dev(channel)	 (channel->port_base + 6)
+#define reg_status(channel)	 (channel->port_base + 7)
+#define reg_cmd(channel)	 (reg_status(channel))
+#define reg_alt_status(channel)  (channel->port_base + 0x206)
+#define reg_ctl(channel)	 reg_alt_status(channel)
+
+/* reg_status寄存器的一些关键位 */
+#define BIT_STAT_BSY	 0x80	      // 硬盘忙
+#define BIT_STAT_DRDY	 0x40	      // 驱动器准备好	 
+#define BIT_STAT_DRQ	 0x8	      // 数据传输准备好了
+
+/* device寄存器的一些关键位 */
+#define BIT_DEV_MBS	0xa0	    // 第7位和第5位固定为1
+#define BIT_DEV_LBA	0x40
+#define BIT_DEV_DEV	0x10
+
+/* 一些硬盘操作的指令 */
+#define CMD_IDENTIFY	   0xec	    // identify指令
+#define CMD_READ_SECTOR	   0x20     // 读扇区指令
+#define CMD_WRITE_SECTOR   0x30	    // 写扇区指令
+
+/* 定义可读写的最大扇区数,调试用的 */
+#define max_lba ((80*1024*1024/512) - 1)	// 只支持80MB硬盘
+
+uint8_t channel_cnt;	   // 按硬盘数计算的通道数
+struct ide_channel channels[2];	 // 有两个ide通道
+
+/* 用于记录总扩展分区的起始lba,初始为0,partition_scan时以此为标记 */
+int32_t ext_lba_base = 0;
+
+uint8_t p_no = 0, l_no = 0;	 // 用来记录硬盘主分区和逻辑分区的下标
+
+struct list partition_list;	 // 分区队列
+
+/* 构建一个16字节大小的结构体,用来存分区表项 */
+struct partition_table_entry {
+   uint8_t  bootable;		 // 是否可引导	
+   uint8_t  start_head;		 // 起始磁头号
+   uint8_t  start_sec;		 // 起始扇区号
+   uint8_t  start_chs;		 // 起始柱面号
+   uint8_t  fs_type;		 // 分区类型
+   uint8_t  end_head;		 // 结束磁头号
+   uint8_t  end_sec;		 // 结束扇区号
+   uint8_t  end_chs;		 // 结束柱面号
+/* 更需要关注的是下面这两项 */
+   uint32_t start_lba;		 // 本分区起始扇区的lba地址
+   uint32_t sec_cnt;		 // 本分区的扇区数目
+} __attribute__ ((packed));	 // 保证此结构是16字节大小
